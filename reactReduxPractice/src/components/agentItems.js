d96093814ba58c0d6e14a838b76900f7f9d4b1ac
Alkaid-seven <fengfeng.awesome@gmail.com>
2016-09-06 17:48:53 +0800

    [react-redux] style the agentItem component

--- reactReduxPractice/src/components/agentItems.js
+++ reactReduxPractice/src/components/agentItems.js
@@ -8,21 +8,28 @@ export default class AgentItem extends Component {
         <div className='agent-items__each'>
           <div className='agent-items__each__icon'></div>
           <div className='agent-items__each__detail-info'>
-            <div>
+            <div className='agent-items__each__detail-info__up'>
               <span>bjstdmngbgr02.thoughtworks.com</span>
-              <span>| idle</span>
-              <span>| 192.168.1.2</span>
-              <span>| /var/lib/cruise-agent</span>
+              <span className='agent-items__each__detail-info__vertical'>|</span>
+              <span>idle</span>
+              <span className='agent-items__each__detail-info__vertical'>|</span>
+              <span>192.168.1.2</span>
+              <span className='agent-items__each__detail-info__vertical'>|</span>
+              <span>/var/lib/cruise-agent</span>
             </div>
-            <div>
-              <span>+ Specify Resources</span>
-              <span>| Resource:</span>
-              <span> ubuntu</span>
-              <span> firefox3</span>
-              <span> core-duo</span>
-              <span>Deny</span>
+            <div className='agent-items__each__detail-info__down'>
+              <span className='agent-items__each__detail-info__specify-resource'>+ Specify Resources</span>
+              <span className='agent-items__each__detail-info__vertical'>|</span>
+              <span className='agent-items__each__detail-info__resource'>Resource:</span>
+              <span className='agent-items__each__detail-info__machine'> ubuntu</span>
+              <span className='agent-items__each__detail-info__machine-times'>&times;</span>
+              <span className='agent-items__each__detail-info__machine'> firefox3</span>
+              <span className='agent-items__each__detail-info__machine-times'>&times;</span>
+              <span className='agent-items__each__detail-info__machine'> core-duo</span>
+              <span className='agent-items__each__detail-info__machine-times'>&times;</span>
             </div>
           </div>
+          <div className='agent-items__each__delay'>Deny</div>
         </div>
       </div>
     )
