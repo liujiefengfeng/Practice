0d4687a5d0a0f9bc6624ee21028e0bf5617cffcf
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-02-28 22:47:00 +0800

    ML: 给node对象添加注释。只有一部分

--- ML/FC/node.py
+++ ML/FC/node.py
@@ -4,6 +4,11 @@
 
 class Node(object):
     def __init__(self, layer_index, node_index):
+        '''
+        构造节点对象。
+        layer_index:节点所在层的编号
+        node_index:节点编号
+        '''
         self.layer_index = layer_index
         self.node_index = node_index
         self.downstream = []
@@ -12,12 +17,29 @@ class Node(object):
         self.delta = 0
 
     def set_output(self, output):
+        '''
+        设置节点的输出值。节点属于输入层会用到此函数。
+        '''
         self.output = output
 
+    def append_downstream_connection(self, conn):
+        '''
+        添加一个到下游节点的连接。
+        '''
+        self.downstream.append(conn)
+
     def append_upstream_connection(self, conn):
+        '''
+        添加一个到上游节点的连接。
+        '''
         self.upstream.append(conn)
 
     def calc_output(self):
+        '''
+        根据式一，计算节点输出
+                     ->  ->
+        y = sigmoid( w * x )
+        '''
         output = reduce(
             lambda ret, conn: ret + conn.upstream_node.output * conn.weight,
             self.upstream,
