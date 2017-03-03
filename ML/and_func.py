f98183e1747865c079ae0d9fe6768d16fdea1110
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-03-03 22:16:33 +0800

    ML: add note for the add function

--- ML/and_func.py
+++ ML/and_func.py
@@ -14,9 +14,15 @@ def get_training_dataset():
     return input_vecs, labels
 
 def train_and_perceptron():
+    '''
+    使用and真值表训练感知器
+    '''
+    # 创建感知器，输入参数为2（因为and是2元函数），激活函数是f
     p = Perceptron(2, f)
+    # 训练，迭代10轮，学习速率为0.1
     input_vecs, labels = get_training_dataset()
     p.train(input_vecs, labels, 10, 0.1)
+    # 返回训练好的感知器
     return p
 
 if __name__ == '__main__':
