41c002e02b4c89985b055d3737fa569cebebdaf8
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-03-12 22:03:20 +0800

    FE: console some attribute of actions field.

--- cerebro-plugin-master/src/index.js
+++ cerebro-plugin-master/src/index.js
@@ -3,8 +3,16 @@
 const plugin = ({term, display, actions}) => {
   // It is your main plugin function
   // do something and call display() with your results
+  console.log(term)
+  console.log(display)
+  console.log(actions)
+  display({
+    title: 'It works!',
+    subtitle: `You entered ${term}`
+  })
 };
 
 module.exports = {
   fn: plugin
+  keyword: 'techmeme'
 }
