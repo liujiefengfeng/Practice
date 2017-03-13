36a083e82b7fb8b1ab0f294f2916f1a5a252caf9
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-03-13 12:45:57 +0800

    FE: fix some syntx error. add ";" in end of the line, and "," in the object

--- cerebro-plugin-master/src/index.js
+++ cerebro-plugin-master/src/index.js
@@ -3,19 +3,17 @@
 const plugin = ({term, display, actions}) => {
   // It is your main plugin function
   // do something and call display() with your results
-  console.log(term)
-  console.log(display)
-  console.log(actions)
+  console.log(term);
+  console.log(display);
+  console.log(actions);
   display({
     title: 'It works!',
-    subtitle: `You entered ${term}`
-    getPreview: () => (
-      <span> 'Hello' </span>
-    )
-  })
+    subtitle: `You entered ${term}`,
+    getPreview: () => `<span> 'Hello' </span>`
+  });
 };
 
 module.exports = {
-  fn: plugin
+  fn: plugin,
   keyword: 'techmeme'
-}
+};
