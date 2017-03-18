ad9a54276be9d07fbe03f82b96be21cfef4a4329
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-03-19 00:50:38 +0800

    FE: fetch stock data. use sz000001 and test-data. success!

--- cerebro-plugin-master/src/index.js
+++ cerebro-plugin-master/src/index.js
@@ -2,9 +2,9 @@
 const { memoize } = require('cerebro-tools');
 
 const stockCode = 'sz000001';
-const getStockData = memoize((stockCode) => (
+const getStockData = memoize(() => (
     fetch(`http://stocks.sina.cn/aj/sh/info?code=${stockCode}`)
-      .then(response = response.json())
+      .then(response => response.json())
   ));
 
 const plugin = ({term, display, actions}) => {
@@ -13,13 +13,15 @@ const plugin = ({term, display, actions}) => {
   display({
     title: 'It works!',
     subtitle: `You entered ${term}`,
-    getPreview: () => `<span> 'Hello' </span>`
   });
 
   getStockData().then(stockData => {
-    console.log(stockData);
+    console.log('stock data', stockData);
     const latestPrice = stockData.data.hq.zuixin;
+    console.log('the latest price', latestPrice)
     display({
+      title: 'It works!',
+      subtitle: `You entered ${term}`,
       getPreview: ()=> `<span>${latestPrice}</span>`
     })
   });
