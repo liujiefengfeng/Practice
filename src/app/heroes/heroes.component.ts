409848ca4895943f5fd430e5245f833840ac4e60
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-07-19 17:19:15 +0200

    jf: inject service and observable data.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heros',
@@ -10,13 +10,19 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
-  heroes = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+   this.heroService.getHeroes()
+     .subscribe(heroes => this.heroes = heroes);
+  }
+
   ngOnInit() {
+    this.getHeroes();
   }
 }
