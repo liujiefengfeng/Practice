1258c169d3abca85a880eefe3ff06ccfb7c68707
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-07-23 11:34:51 +0200

    ng-demo: get heroes with HttpClient.

--- ng-demo/src/app/hero.service.ts
+++ ng-demo/src/app/hero.service.ts
@@ -3,19 +3,23 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService} from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+    // get heroes from the server
   getHeroes(): Observable<Hero[]> {
 
     //TODO: send the message _after_ fetching the heros
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
@@ -23,4 +27,11 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
 }
