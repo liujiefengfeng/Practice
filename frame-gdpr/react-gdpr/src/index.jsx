09aeaac473e89ae63259001735bbc4b74824e63a
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-06 14:49:52 +0200

    frame-react: upgrade material-ui

--- frame-gdpr/react-gdpr/src/index.jsx
+++ frame-gdpr/react-gdpr/src/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
 import Search from './page/Search.jsx';
 import './index.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <RaisedButton label="Test Material-ui"/>
+      <div>
         <Search/>
-      </MuiThemeProvider>
+      </div>
     );
   }
 }
