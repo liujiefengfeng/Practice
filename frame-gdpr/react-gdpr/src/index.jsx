f2d18d3886c4ca6fcc216c5c1dc311d12fffbe60
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-06 10:26:19 +0200

    frame: initial the project

---
+++ frame-gdpr/react-gdpr/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div id='hello-world'>Hello World!</div>
+    );
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
---
+++ frame-gdpr/react-gdpr/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new HtmlWebpackPlugin({
+      title: "react gdpr",
+      template: "src/index.html"
+    })],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
