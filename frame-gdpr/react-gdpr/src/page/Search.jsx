8a0fdeea2ca2be91948a6891f2754f3ff742ca6e
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-06 11:47:28 +0200

    frame-react: add vertical Navigation in Search page.

--- frame-gdpr/react-gdpr/src/page/Search.jsx
+++ frame-gdpr/react-gdpr/src/page/Search.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Search extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
   render() {
-    return (<div>Hello</div>)
+    return (
+      <div>
+        <RaisedButton
+          label="Toggle Drawer"
+          onClick={this.handleToggle}
+        />
+        <Drawer width={200}
+                open={this.state.open}
+                onRequestChange={(open) => this.setState({open})}>
+          <AppBar title="GDPR"/>
+          <MenuItem onClick={this.handleClose}>Search</MenuItem>
+          <MenuItem onClick={this.handleClose}>Add New</MenuItem>
+        </Drawer>
+      </div>
+    );
   }
 }
 
