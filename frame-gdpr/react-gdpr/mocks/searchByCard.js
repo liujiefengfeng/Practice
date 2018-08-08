92775f52e8984e85be2eda4132ba61f7088441e2
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-08 12:24:16 +0200

    frame-react: customized the mock data

--- frame-gdpr/react-gdpr/mocks/searchByCard.js
+++ frame-gdpr/react-gdpr/mocks/searchByCard.js
@@ -1,7 +1,30 @@
 module.exports = {
-  path: '/infos/cardnumber',
+  path: '/infos/:cardNumber',
   template: {
+    cardNumber: params => String(params.cardNumber),
     name: 'fengfeng',
-    email: 'fengfeng.awesome@gmail.com'
+    email: 'fengfeng.awesome@gmail.com',
+    customer: {
+      customerId: '001000',
+      customerName: 'ThoughtWorks',
+      customerAddress: {
+        country: 'German',
+        city: 'Hamburg',
+        streetAndNumber: 'Großer Burstah 46',
+        zipCode: '20457'
+      }
+    },
+    employees: [
+      {
+        employeeId:'001001',
+        employeeName: 'amber awesome',
+        employeeEmail: 'aawesome@gmail.com'
+      },
+      {
+        employeeId:'001002',
+        employeeName: 'aisha first',
+        employeeEmail: 'afirst@gmail.com'
+      }
+    ]
   }
 };
