c00bf5be3172b6d4e3771225d3ee0d285066b3d2
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-13 10:40:44 +0200

    ng-demo: 修改heroesComponent并且注入heroService。

--- ng-demo/src/app/heroes/heroes.component.ts
+++ ng-demo/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +9,9 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
