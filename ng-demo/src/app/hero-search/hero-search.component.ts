ff8712d03eb2f6a0ad3ab42b2254216b237cad72
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-15 12:52:21 +0200

    ng-demo: 实现搜索功能。

--- ng-demo/src/app/hero-search/hero-search.component.ts
+++ ng-demo/src/app/hero-search/hero-search.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from "rxjs/index";
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/internal/operators";
+import { Hero } from '../hero';
+import { HeroService } from "../hero.service";
+
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
   styleUrls: ['./hero-search.component.css']
 })
+
 export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
 
-  constructor() { }
+  private searchTerms = new Subject<string>();
 
-  ngOnInit() {
+  constructor(private heroService: HeroService) {
   }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
 }
