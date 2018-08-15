d973c02f0c75c3e10df8f2438a010c51abb97ce0
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-15 11:31:02 +0200

    ng-demo: 添加新英雄。

--- ng-demo/src/app/heroes/heroes.component.ts
+++ ng-demo/src/app/heroes/heroes.component.ts
@@ -21,4 +21,15 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
+
+  addHero(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      })
+  }
 }
