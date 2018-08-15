a6d1ae6923affbe3eefea11a4514df036f44bd8a
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-15 11:14:22 +0200

    ng-demo: 添加 updateHero 选项。

--- ng-demo/src/app/hero.service.ts
+++ ng-demo/src/app/hero.service.ts
@@ -43,6 +43,14 @@ export class HeroService {
       );
   }
 
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap(_ => this.log(`update hero id=${hero.id}`)),
+        catchError(this.handleError<Hero>(`updateHero id=${hero.id}`))
+      );
+  }
+
   /**
    * Handle Http operation that failed.
    * Let the app continue.
