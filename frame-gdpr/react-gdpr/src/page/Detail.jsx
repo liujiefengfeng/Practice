1c10d5b30fbc82a9bc7bcc6949f613543df05714
liujiefengfeng <fengfeng.awesome@gmail.com>
2018-08-20 10:18:07 +0200

    react-gdpr: add tab for list page.

--- frame-gdpr/react-gdpr/src/page/Detail.jsx
+++ frame-gdpr/react-gdpr/src/page/Detail.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class Detail extends React.Component {
   render() {
     return (
       <div>
-        Detail
+        <AppBar>
+          <Tabs>
+            <Tab label="Company Detail"/>
+            <Tab label="Personal Information"/>
+          </Tabs>
+        </AppBar>
       </div>
     )
   }
