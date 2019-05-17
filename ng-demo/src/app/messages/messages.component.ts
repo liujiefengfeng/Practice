c4cddaa08cd8389e5146e3d55c0e4f717a5af918
liujiefengfeng <fengfeng.awesome@gmail.com>
2019-05-17 11:06:38 +0800

    base test

--- ng-demo/src/app/messages/messages.component.ts
+++ ng-demo/src/app/messages/messages.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { MessageService } from "../message.service";
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnChanges {
 
+  @Input() hei:String;
+  @Input() config;
+
+  private configCopy;
   constructor(public messageService: MessageService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    console.log('MessagesComponent');
+    console.log(changes);
+    console.log(changes.config.currentValue);
+    this.configCopy = cloneDeep(changes.config.currentValue);
   }
 
 }
