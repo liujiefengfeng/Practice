99253b5a417fe9ba5df7194021656d34fbb4e887
liujiefengfeng <fengfeng.awesome@gmail.com>
2020-05-17 13:12:18 -0700

    designpattern: builder

---
+++ designPattern/src/builder/builder.ts
@@ -0,0 +1,57 @@
+enum CarTypes {
+  truck = "TRUCK",
+  sedan = "SEDAN",
+  coupes = "COUPES",
+  unknown = "UNKNOWN"
+}
+
+class Car {
+  type: CarTypes;
+  color: string;
+  wheels: number;
+
+  constructor(type: CarTypes, color: string, wheels: number) {
+    this.type = type;
+    this.color = color;
+    this.wheels = wheels;
+  }
+
+  info() {
+    console.log("car type: ", this.type);
+    console.log("car color: ", this.color);
+    console.log("car wheels: ", this.wheels);
+  }
+}
+
+class CarBuilder {
+  private type = CarTypes.unknown;
+  private color = "";
+  private wheels = 0;
+
+  setCarType(type: CarTypes) {
+    this.type = type;
+    return this;
+  }
+
+  setCarColor(color: string) {
+    this.color = color;
+    return this;
+  }
+
+  setWheels(wheels: number) {
+    this.wheels = wheels;
+    return this;
+  }
+
+  build(): Car {
+    return new Car(this.type, this.color, this.wheels);
+  }
+}
+
+const truckCar: Car = new CarBuilder()
+.setCarType(CarTypes.truck)
+.setCarColor("red")
+.setWheels(4)
+.build();
+
+truckCar.info()
