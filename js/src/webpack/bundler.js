98aa458bbca9c37e295e5b70a36fe2d619f25659
liujiefengfeng <fengfeng.awesome@gmail.com>
2020-05-23 14:39:59 -0700

    js: webpack step3 done

--- js/src/webpack/bundler.js
+++ js/src/webpack/bundler.js
@@ -10,7 +10,7 @@ function stepOne(filename) {
     sourceType: 'module' // ES module
   });
   
-  const dependencies = [];
+  const dependencies = {};
   
   //遍历AST抽象语法树
   traverse(ast, {
@@ -63,4 +63,35 @@ function stepTwo(entry) {
   return graph;
 }
 
-console.log(stepTwo('./index.js'));
+// console.log(stepTwo('./index.js'));
+
+function stepThree(entry) {
+  const graph = JSON.stringify(stepTwo(entry));
+  
+  return `
+  (function (graph) {
+    //require函数的本质是执行一个模块的代码，然后将相应变量挂载到exports对象上
+    function require(module) {
+      //localRequire的本质是拿到依赖包的exports变量
+      function localRequire(relativePath) {
+        return require(graph[module].dependencies[relativePath]);
+      }
+      
+      var exports = {};
+      (function (require, exports, code) {
+        console.log("..................")
+        console.log(code)
+        eval(code);
+        console.log("..................")
+      })(localRequire, exports, graph[module].code);
+      
+      //函数返回指向局部变量，形成闭包，exports变量在函数执行后不会被摧毁
+      return exports;
+    }
+    
+    require('${entry}')
+  })(${graph})
+  `
+}
+
+console.log(stepThree('./index.js'));
