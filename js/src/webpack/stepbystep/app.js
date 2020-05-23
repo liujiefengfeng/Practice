df7feced0cb748a4e7aef8b85d9a2b96f85fc8f3
liujiefengfeng <fengfeng.awesome@gmail.com>
2020-05-23 16:06:16 -0700

    js: webpack - entry/output/cleandistplugin

---
+++ js/src/webpack/stepbystep/app.js
@@ -0,0 +1,14 @@
+// ES6
+import sum from './vendor/sum'
+
+console.log(sum(1, 2));
+
+
+// CommonJS
+var minus = require("./vendor/minus");
+console.log(minus(1, 2));
+
+// AMD
+// require(['./vendor/multi'], function(multi) {
+//   console.log(multi(1, 2))
+// })
