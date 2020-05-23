f10f784900dce6bac827d1792a66e954db1937dc
liujiefengfeng <fengfeng.awesome@gmail.com>
2020-05-23 16:48:11 -0700

    js webpack html

--- js/src/webpack/stepbystep/webpack.config.js
+++ js/src/webpack/stepbystep/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,7 +8,7 @@ module.exports = {
   },
   
   output: {
-    publicPath: __dirname + '/dist/', // js 引用的路径或者 CDN 地址
+    publicPath: './', // js 引用的路径或者 CDN 地址
     path: path.resolve(__dirname, 'dist'), // 打包文件的输出目录
     filename: '[name].bundle.js', // 打包后生产的 js 文件
     chunkFilename: '[name].js' // 代码拆分后的文件名
@@ -25,7 +26,19 @@ module.exports = {
   },
   
   plugins: [
-    new CleanWebpackPlugin() // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
+    new CleanWebpackPlugin(), // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
+    new HtmlWebpackPlugin({
+      // 打包输出HTML
+      title: '自动生成 HTML',
+      minify: {
+        // 压缩 HTML 文件
+        removeComments: true, // 移除 HTML 中的注释
+        collapseWhitespace: true, // 删除空白符与换行符
+        minifyCSS: true // 压缩内联 css
+      },
+      filename: 'index.html', // 生成后的文件名
+      template: 'index.html' // 根据此模版生成 HTML 文件
+    })
   ],
   
   module: {
